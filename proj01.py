a14f613232b76ba2cb095c46a7c0d218c70e92ef
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-11 15:38:04 -0500

    Add files via upload

--- proj01.py
+++ proj01.py
@@ -20,22 +20,17 @@ print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in
 
 day = int(raw_input("What day of the month were you born on?"))
 month = int(raw_input("What month were you born in?"))
+
+if month == current_month:
+    print "Your birthday is in about 11 months "
 if month < current_month:
     print "Your birthday is in about " + str(month - current_month + 12) + " months "
 else:
     print "Your birthday is in about " + str(month - current_month) + " months "
 if day < current_day:
-    print "and " + str(day - current_day + 30) + "days."
+    print "and " + str(day - current_day + 30) + " days."
 else:
     print "and " + str(day - current_day) + " days."
-
-# Extensions:
-
-print "Now I will tell you what types of movies you can see. Everyone can see a G rated movie."
-age = int(raw_input("How old are you, in years?"))
-
-
-
 
 
 # If you complete extensions, describe your extensions here!
