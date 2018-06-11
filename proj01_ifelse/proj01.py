333dfd09e46fc997ed74fd7e6df6a171e5079ce3
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-11 15:39:37 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -10,7 +10,7 @@ current_month = int(6)
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
 
-name = raw_input("What is your name, my dude?")
+name = raw_input("What is your name?")
 grade =  int(raw_input("What grade are you in?"))
 print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in " + str(12 - grade) + " years."
 
@@ -20,13 +20,17 @@ print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in
 
 day = int(raw_input("What day of the month were you born on?"))
 month = int(raw_input("What month were you born in?"))
-if month < current_month:
-    print "Your birthday is in " + str(month - current_month + 12) + " months "
-else:
-    print "Your birthday is in " + str(month - current_month) + " months "
-if day < current_day:
-    print "and " + str(day - current_day) + "days."
 
+if month == current_month:
+    print "Your birthday is in about 11 months "
+if month < current_month:
+    print "Your birthday is in about " + str(month - current_month + 12) + " months "
+else:
+    print "Your birthday is in about " + str(month - current_month) + " months "
+if day < current_day:
+    print "and " + str(day - current_day + 30) + " days."
+else:
+    print "and " + str(day - current_day) + " days."
 
 
 # If you complete extensions, describe your extensions here!
