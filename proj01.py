84efdf6d32f492f86c2f5bdf236751705ef95ca6
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-11 14:32:41 -0500

    Add files via upload

---
+++ proj01.py
@@ -0,0 +1,33 @@
+# Name:
+# Date:
+
+# proj01: A Simple Program
+
+current_day = int(11)
+current_month = int(6)
+
+# Part I:
+# This program asks the user for his/her name and grade.
+#Then, it prints out a sentence that says the number of years until they graduate.
+
+name = raw_input("What is your name, my dude?")
+grade =  int(raw_input("What grade are you in?"))
+print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in " + str(12 - grade) + " years."
+
+# Part II:
+# This program asks the user for his/her name and birth month.
+# Then, it prints a sentence that says the number of days and months until their birthday
+
+day = int(raw_input("What day of the month were you born on?"))
+month = int(raw_input("What month were you born in?"))
+if month < current_month:
+    print "Your birthday is in " + str(month - current_month + 12) + " months "
+else:
+    print "Your birthday is in " + str(month - current_month) + " months "
+if day < current_day:
+    print "and " + str(day - current_day) + "days."
+
+
+
+# If you complete extensions, describe your extensions here!
+
