7f07e03e09e2dabc0556fba3f5c0abf8f154f839
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-11 14:54:12 -0500

    Add files via upload

--- proj01.py
+++ proj01.py
@@ -10,7 +10,7 @@ current_month = int(6)
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
 
-name = raw_input("What is your name, my dude?")
+name = raw_input("What is your name?")
 grade =  int(raw_input("What grade are you in?"))
 print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in " + str(12 - grade) + " years."
 
@@ -21,11 +21,20 @@ print (name[1].upper() + name[2:].lower()) + " will graduate from high-school in
 day = int(raw_input("What day of the month were you born on?"))
 month = int(raw_input("What month were you born in?"))
 if month < current_month:
-    print "Your birthday is in " + str(month - current_month + 12) + " months "
+    print "Your birthday is in about " + str(month - current_month + 12) + " months "
 else:
-    print "Your birthday is in " + str(month - current_month) + " months "
+    print "Your birthday is in about " + str(month - current_month) + " months "
 if day < current_day:
-    print "and " + str(day - current_day) + "days."
+    print "and " + str(day - current_day + 30) + "days."
+else:
+    print "and " + str(day - current_day) + " days."
+
+# Extensions:
+
+print "Now I will tell you what types of movies you can see. Everyone can see a G rated movie."
+age = int(raw_input("How old are you, in years?"))
+
+
 
 
 
