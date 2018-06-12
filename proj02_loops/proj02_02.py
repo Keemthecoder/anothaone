0f791dccc394e7ba3764c4fb4dd2b431e26ed9ea
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-12 14:38:16 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,18 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+previous = 0
+current = 1
+next = int(previous + current)
+times = int(raw_input("How many Fibonacci numbers do you want?"))
+for number in range(0, times):
+    print current
+    next = int(previous + current)
+    previous = current
+    current = next
 
+previous2 = 2
+times2 = int(raw_input("How many powers of 2 do you want?"))
+for number in range(0, times2):
+    print previous2
+    previous2 = previous2 * 2
