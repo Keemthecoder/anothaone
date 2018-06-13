82715feb71e285bf9837fb2e7bd9cb17d1e80088
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-13 11:38:40 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -12,21 +12,29 @@ practice with lists
 #Part I
 #Take a list, say for example this one:
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #and write a program that prints out all the elements of the list that are less than 5.
 
+for list1 in range(0,5):
+    print list1
 
+for list1 in range(0,5):
+    list1b = list1[0,5]
+    print list1b[0]
 
-
-
+y = int(raw_input("How many numbers you want boi?"))
+for list1 in range(0,y):
+    print list1
 
 #Part II
 # Take two lists, say for example these two:
-b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+list2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # and write a program that creates and prints a list that contains only the elements
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
+
+
 
 
 
