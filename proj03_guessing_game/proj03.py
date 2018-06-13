72d88572b41412f6f32fb389bca720759d45c554
Keemthecoder <40177200+Keemthecoder@users.noreply.github.com>
2018-06-13 09:19:19 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -12,4 +12,25 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 
 """
 
+import random
+var = random.randint(1,10)
+guess = 0
+guessnumber = int(raw_input("How many guesses do you want?"))
+for x in range(0, guessnumber):
+    guess = int(raw_input("Guess a number between 1 and 10, including 1 and 10."))
+    if guess == var:
+        print "Congrats, you guessed the number!"
+        break
+    elif guess < var:
+        print "Too low!"
+    elif guess > var:
+        print "Too high!"
+    guessnumber = guessnumber - 1
+    print "You have " + str(guessnumber) + " tries left."
+    if guessnumber == 0:
+        print "You weren't able to guess the number! Try again!"
+
+
+
+
 
